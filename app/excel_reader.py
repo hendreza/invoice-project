6f43b8f3101be6d7835984c8c8678cd14a5df18c
Hendre <hendre.za@gmail.com>
2026-04-21 11:24:55 +0200

    end of day 2

--- app/excel_reader.py
+++ app/excel_reader.py
@@ -3,9 +3,13 @@
 import pandas as pd
 
 
-file_path = "/home/hendre/Documents/code/invoice-project/input/invoice.xlsx"
+file_path = "/home/hendre/Documents/Code/invoice-project/input/invoice.xlsx"
 file_name = pd.ExcelFile(file_path)
 sheet = "invoice info"
 
 df = pd.read_excel(file_name, sheet_name=sheet)
-print(df.head())
+
+for index, row in df.iterrows():
+    if index == 0:
+        print(row["invoice_number"], "|", row["invoice_date"], "|", row["name"], "|", row["last_name"], "|", row["description"], "|",  row["total"])
+
